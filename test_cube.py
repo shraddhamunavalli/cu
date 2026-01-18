c7a0673dca83bc6c51038312a1aa76530443d5a3
shraddhamunavalli <munavallishraddha4@gmail.com>
2026-01-18 17:14:01 +0530

    Fix test for cube function output

--- test_cube.py
+++ test_cube.py
@@ -1,7 +1,5 @@
+import cube
+
 def test_cube():
-    excepted_output = (
-        "Enter a number: 2 \n"
-        "cube = num ** 3\n"
-        "print(f\"Cube of {num} is {cube}\")\n"
-    )
-    assert cube.main("2") == excepted_output
+    expected_output = "Cube of 2 is 8.0"
+    assert cube.main("2") == expected_output
